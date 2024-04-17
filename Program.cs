a266222aa9fa95b23e0483e1cb62b10d5dd41994
George Tsyrkov <g@tias.pro>
2024-04-17 13:25:16 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tsyrkov.Tgen;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2 || args.Length == 0) { PrintUsage(); }
+
+        
+
+    }
+
+    public static void PrintUsage()
+    {
+        Console.WriteLine("USAGE:");
+        Console.WriteLine("tgen [template file path] [values in JSON file path]");
+    }
+}
